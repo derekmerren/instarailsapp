c7fdeae8ae45c691b5136067750faeb05dc16f61
Derek Merren <derekmerren@Dereks-MacBook-Air.local>
2016-12-10 14:20:42 +0000

    Changed back to initializer with require statement.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,12 +4,12 @@ class PicturesController < ApplicationController
   # GET /pictures
   # GET /pictures.json
   def index
-    Instagram.configure do |config|
-      config.client_id = "7a9907fe3000499f8f67e54e2fa47df4"
-      config.client_secret = "25a176d74547425dab2fdcdd339bf96c"
-      # For secured endpoints only
-      #config.client_ips = '<Comma separated list of IPs>'
-    end
+    # Instagram.configure do |config|
+    #   config.client_id = "7a9907fe3000499f8f67e54e2fa47df4"
+    #   config.client_secret = "25a176d74547425dab2fdcdd339bf96c"
+    #   # For secured endpoints only
+    #   #config.client_ips = '<Comma separated list of IPs>'
+    # end
     access_token = params[:code]
     client = Instagram.client(:access_token => access_token)
     @pictures = client.user_recent_media
