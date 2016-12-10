98d1cdfdc86a44495ae105fb5b5798afae1abcfa
Derek Merren <derekmerren@Dereks-MacBook-Air.local>
2016-12-10 14:12:37 +0000

    Added instagram initializer

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,6 @@
+Instagram.configure do |config|
+  config.client_id = "7a9907fe3000499f8f67e54e2fa47df4"
+  config.client_secret = "25a176d74547425dab2fdcdd339bf96c"
+  # For secured endpoints only
+  #config.client_ips = '<Comma separated list of IPs>'
+end
