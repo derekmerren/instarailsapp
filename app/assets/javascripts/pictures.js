4c921e79d60c8a7c01e43ba2713269ec2aba8336
Derek Merren <derekmerren@unknown.home>
2016-12-11 19:35:50 +0000

    Removed console.logs.

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -5,11 +5,9 @@ $( document ).ready(function() {
   $('#zoom-modal').on('show.bs.modal', function (event) {
     console.log('modal opened')
     var image = $(event.relatedTarget) // Button that triggered the modal
-    var bigImageUrl = image.data('image-large-url') // Extract info from data-* attributes
+    // Extract info from data-* attributes
+    var bigImageUrl = image.data('image-large-url')
     var imageCaption = image.data('image-caption')
-    console.log(bigImageUrl)
-    console.log(imageCaption)
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this)
     modal.find('.modal-title').text(imageCaption)
     modal.find('.modal-body img').attr('src', bigImageUrl)
